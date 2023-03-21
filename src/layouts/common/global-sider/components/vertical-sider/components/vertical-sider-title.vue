c04e72552243d6f4b77324acd8234c685dc3bdc7
yyh <362127358@qq.com>
2023-03-21 18:06:11 +0800

    feat(components):调整 vertical-sider-title 样式

--- src/layouts/common/global-sider/components/vertical-sider/components/vertical-sider-title.vue
+++ src/layouts/common/global-sider/components/vertical-sider/components/vertical-sider-title.vue
@@ -1,6 +1,11 @@
 <template>
-  <router-link v-show="showTitle" :to="routeHomePath" class="flex-center w-full nowrap-hidden">
-    <system-logo class="text-32px text-primary" />
+  <router-link
+    v-show="showTitle"
+    :to="routeHomePath"
+    class="flex-center w-full nowrap-hidden pl-16px"
+    style="justify-content: left; box-shadow: 0 1px 2px rgb(0 21 41 / 8%)"
+  >
+    <!-- <system-logo class="text-32px text-primary" /> -->
     <h2 class="pl-8px text-16px font-bold text-primary transition duration-300 ease-in-out" @click="toggleLocal">
       {{ title }}
     </h2>
@@ -32,7 +37,6 @@ function toggleLocal() {
 }
 
 const title = computed(() => {
-  debugger;
   return routeStore.siderMenus?.label;
 });
 </script>
